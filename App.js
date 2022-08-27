91a8b9c68d70d8ab0fefdbd7f907a18f5d314c55
Angkana Boonya <mayangkanaboonya@gmail.com>
2022-08-27 23:25:30 +0700

    Connect (4)

--- App.js
+++ App.js
@@ -1,32 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { StyleSheet, View ,ImageBackground} from 'react-native';
-import Weather from './components/Weather';
-import Constants from 'expo-constants';
+import { StyleSheet, View ,ImageBackground } from 'react-native-web';
+export default function App() {
+ const doIt = () => {
+ console.log("Hello from console")
+ }
+ return (
 
-
-export default function App() { 
-
-
-return (
-        <View style={styles.container}>
-      <View style={styles.container}>
-        <Weather zipCode="90110"/>
-        <StatusBar style="auto" />
-
-      </View>
-  );
-
+ <View style={styles.container}>
+ <Weather zipCode="90110"/>
+ <StatusBar style="auto" />
+ </View>
+ );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: Constants.statusBarHeight
-const styles = StyleSheet.create(
-  {
-    container: {
-    paddingTop: Constants.statusBarHeight,
-    },
-  }
-);
+ container: {
+  paddingTop: Constants.StatusBarHeight
+ },
+});
+
